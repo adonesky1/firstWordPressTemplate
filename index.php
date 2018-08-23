e6cfbc02c97cbb576864c99d09dc86a4fb15d783
Alex Donesky <adonesky@gmail.com>
2018-08-23 17:06:30 -0500

    add comments and pagination

--- index.php
+++ index.php
@@ -4,22 +4,21 @@
 
         <div class="col-sm-8 blog-main">
 
-            <?php if ( have_posts() ) : 
-                  while ( have_posts() ) : 
-                    the_post();
-                  
-                    get_template_part( 'content', get_post_format() );
-  
-            endwhile ?>
-            <nav>
-                  <ul class="pager">
-                        <li><?php next_posts_link('Previous'); ?></li>
-                        <li><?php previous_posts_link('Next'); ?></li>
-                  </ul>
-            </nav>
-          <?php
-            endif; 
-		 ?>
+           <?php 
+            if (have_posts()) : 
+                  while (have_posts()) : the_post();
+            
+                  get_template_part('content', get_post_format());
+            endwhile; ?>
+                        
+                  <nav>
+                        <ul class="pager">
+                              <li><?php next_posts_link('Previous'); ?></li>
+                              <li><?php previous_posts_link('Next'); ?></li>
+                        </ul>
+                  </nav>
+
+            <?php endif; ?>
 
         </div><!-- /.blog-main -->
 
